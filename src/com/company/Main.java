aa314c3a5958c4b2bf3b0bba3ab039aace38851a
Georgiana <stativa50@gmail.com>
2022-04-30 21:57:55 +0300

    Parser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,51 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Parser parser = new Parser();
 
-        BigNumber number = new BigNumber("1234141588");
-        BigNumber number2 = new BigNumber("100");
+        while (true) {
 
-        System.out.println(BigNumber.raisedTo(number, number2));
+            System.out.println("Please enter the input mode: 1(console) or 2(from file)");
+
+            Scanner scanner = new Scanner(System.in);
+
+            String userMode = scanner.nextLine();
+
+            if (userMode.equals("1")) {
+
+                System.out.println("Please enter the expression:");
+
+                String expression = scanner.nextLine();
+
+                Set<Character> characterList = parser.getVariables(expression);
+
+                Map<Character, BigNumber> values = new HashMap<>();
+
+                for (Character character : characterList) {
+
+                    System.out.println(String.format("Please enter value for %s:", character));
+
+                    String number = scanner.nextLine();
+
+                    values.put(character, new BigNumber(number));
+                }
+
+                System.out.println(parser.execute(expression, values));
+
+            } else if (userMode.equals("2")) {
+
+                //read from file
+            } else {
+
+            }
+        }
     }
 }
---
+++ src/com/company/OperatorsEnum.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class OperatorsEnum {
+
+    public static final String ADD = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVISION = "/";
+    public static final String POW = "^";
+    public static final String SQRT = "S";
+
+
+    private OperatorsEnum() {
+
+    }
+}
