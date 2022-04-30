d8f6fedd544ea4c7338680b95bff6b8a6db0e674
Rusu <rusudaniel5799@gmail.com>
2022-04-30 15:43:04 +0300

    add/subtract/compare/multiply/trailingZeros

---
+++ src/com/company/BigNumber.java
@@ -0,0 +1,152 @@
+package com.company;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BigNumber {
+
+    private final Integer totalDigits;
+    private final List<Integer> digits;
+
+    public BigNumber(String number) {
+
+        digits = new ArrayList<>();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(number);
+        builder.reverse();
+        number = builder.toString();
+
+        digits.addAll(number.chars()
+                .mapToObj(c -> Integer.parseInt(String.valueOf((char) c)))
+                .collect(Collectors.toList()));
+
+        totalDigits = digits.size();
+    }
+
+    public BigNumber add(BigNumber number) {
+
+        List<Integer> numberDigits = number.digits;
+
+        StringBuilder resultBuilder = new StringBuilder();
+
+        int carry = 0;
+        for(int i = 0; i < numberDigits.size() || i < digits.size() || carry != 0; i++) {
+
+            int resultDigit = carry;
+
+            if(digits.size() > i) resultDigit += digits.get(i);
+            if(numberDigits.size() > i) resultDigit += numberDigits.get(i);
+
+            carry = resultDigit / 10;
+            resultDigit %= 10;
+
+            resultBuilder.append(resultDigit);
+        }
+
+        return new BigNumber(resultBuilder.reverse().toString());
+    }
+
+    public BigNumber subtract(BigNumber number) {
+
+        if(compare(number) < 0) {
+
+            throw new InvalidParameterException("First number smaller than second at subtract");
+        }
+
+        List<Integer> numberDigits = number.digits;
+
+        StringBuilder resultBuilder = new StringBuilder();
+
+        int carry = 0;
+        for(int i = 0; i < digits.size(); i++) {
+
+            int resultDigit = digits.get(i)  - carry;
+
+            if(numberDigits.size() > i) resultDigit -= numberDigits.get(i);
+
+            if(resultDigit < 0) {
+
+                carry = 1;
+                resultDigit += 10;
+            }
+
+            resultBuilder.append(resultDigit);
+        }
+
+        return new BigNumber(resultBuilder.reverse().toString()).removeTrailingZeros();
+    }
+
+    public BigNumber multiply(BigNumber number) {
+
+        List<Integer> numberDigits = number.digits;
+
+        int[] result = new int[number.totalDigits + totalDigits];
+
+        for(int index1 = 0; index1 < totalDigits; index1++) {
+            for(int index2 = 0; index2 < number.totalDigits; index2++) {
+
+                result[index1 + index2] += digits.get(index1) * number.digits.get(index2);
+            }
+        }
+
+        int carry = 0;
+        StringBuilder resultBuilder = new StringBuilder();
+        for(int index = 0; index < totalDigits + number.totalDigits - 1 || carry != 0; index++) {
+
+            int resultDigit = carry;
+
+            if(index < totalDigits + number.totalDigits - 1) resultDigit += result[index];
+
+            carry = resultDigit / 10;
+            resultDigit %= 10;
+
+            resultBuilder.append(resultDigit);
+        }
+
+        return new BigNumber(resultBuilder.reverse().toString());
+    }
+
+    private int compare(BigNumber number) {
+
+        if(!number.totalDigits.equals(totalDigits)) {
+
+            return (totalDigits < number.totalDigits ? -1 : 1);
+        }
+
+        for(int index = totalDigits - 1; index >= 0; index--) {
+
+            if(!digits.get(index).equals(number.digits.get(index))) {
+
+                return digits.get(index).compareTo(number.digits.get(index));
+            }
+        }
+
+        return 0;
+    }
+
+    private BigNumber removeTrailingZeros() {
+
+        while(digits.size() > 1 && digits.get(digits.size() - 1) == 0) {
+
+            digits.remove(digits.size() - 1);
+        }
+
+        return new BigNumber(toString());
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int index = digits.size() - 1; index >= 0; index--) {
+
+            builder.append(digits.get(index));
+        }
+
+        return builder.toString();
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        BigNumber number = new BigNumber("9999");
+        BigNumber number2 = new BigNumber("9999");
+
+        System.out.println(number.multiply(number2));
+    }
+}
