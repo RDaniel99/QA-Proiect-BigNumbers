126c7740f01e43598382ce1eff0d5a9fd4e320d1
Rusu <rusudaniel5799@gmail.com>
2022-04-30 18:58:37 +0300

    added Parser

---
+++ src/com/company/Parser.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Map;
+
+public class Parser {
+
+    public BigNumber execute(String expression, Map<String, BigNumber> values) {
+
+        throw new NotImplementedException();
+    }
+}
